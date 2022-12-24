5a5faa222cc536fe0186baef212b0b0fce1f6c32
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-12-24 14:14:36 +0530

    Made changes to add cookie to login

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -46,7 +46,7 @@ export default function Dashboard(){
                 </div>}
         {game.length !== 0 && <div style={{marginTop: "20px"}}>{game.map((key)=>(
             <Singlegame user1 = {key.user1} user2 = {key.user2} current = {key.current}  winby = {key.winby} board = {key.board} time = {key.time} id = {key._id}/>
-        ))}<p className="newGameText" onClick={()=>(navigate("/newgame"))}><span style={{color: "white"}}>+ New Game</span></p></div>}
+        ))}<p className="newGameText" onClick={()=>(navigate("/newgame"))}><span style={{color: "white", cursor: "pointer"}}>+ New Game</span></p></div>}
         </div>}
         </div>
     )
