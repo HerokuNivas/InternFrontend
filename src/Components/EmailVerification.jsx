ff0ba0a313ac2484722a2a4128352a6a2ba59249
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-12-26 15:22:36 +0530

    Made cahnges

--- src/Components/EmailVerification.jsx
+++ src/Components/EmailVerification.jsx
@@ -10,7 +10,7 @@ export default function EmailVerification(){
             Username : "asynctictactoe@gmail.com",
             Password : "A41716B46FDA1AD53D57045901ACB253905E",
             // SecureToken : "3d7638a5-656f-4a2c-a4a8-858cacaa042a",
-            To: "vsnsainivasand2003@gmail.com",
+            To: "202051197@iiitvadodara.ac.in",
             From: "asynctictactoe@gmail.com",
             Subject: "OTP for Verification",
             Body: "Hello,\nPlease enter the following OTP to proceed further [OTP].\n\nThanks for using Asynchronous Tic Tac Toe,\nTeam CHINXTUS."
