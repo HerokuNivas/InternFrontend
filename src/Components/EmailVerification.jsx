3cbb50e900f057aec120689440da1cc191715ad4
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-12-26 15:15:43 +0530

    Mail added

--- src/Components/EmailVerification.jsx
+++ src/Components/EmailVerification.jsx
@@ -6,11 +6,14 @@ export default function EmailVerification(){
     useEffect(()=>{
         // eslint-disable-next-line no-undef
         Email.send({
-            SecureToken : "3d7638a5-656f-4a2c-a4a8-858cacaa042a",
+            Host : "smtp.elasticemail.com",
+            Username : "asynctictactoe@gmail.com",
+            Password : "A41716B46FDA1AD53D57045901ACB253905E",
+            // SecureToken : "3d7638a5-656f-4a2c-a4a8-858cacaa042a",
             To: "vsnsainivasand2003@gmail.com",
             From: "asynctictactoe@gmail.com",
-            Subject: "OTP for Asynchronous Tic Tac Toe",
-            Body: "Hello,Please enter the following OTP to proceed further [OTP].Thanks for using Asynchronous Tic Tac Toe,Team CHINXTUS."
+            Subject: "OTP for Verification",
+            Body: "Hello,\nPlease enter the following OTP to proceed further [OTP].\n\nThanks for using Asynchronous Tic Tac Toe,\nTeam CHINXTUS."
         }).then(
           message => alert(message)
         );
