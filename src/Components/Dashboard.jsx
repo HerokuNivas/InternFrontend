fe3e4f36f7e421d9c2f9fd8c2709208a79549a09
HerokuNivas <heroku.nivas.deployments@gmail.com>
2023-02-08 22:22:28 +0530

    Added animated side bar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -11,6 +11,8 @@ import Singlegame from "./Singlegame";
 import LogoutIcon from '@mui/icons-material/Logout';
 import video1 from "../Animations/Dashboard.mp4";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 
 export default function Dashboard(){
     let navigate = useNavigate();
@@ -18,6 +20,7 @@ export default function Dashboard(){
     const [game, setGames] = useState([]);
     const [loading, setLoading] = useState(false);
     const [requests, setRequests] = useState("0");
+    const [expandClick, setExpandClicked] = useState(false);
 
     useEffect(()=>{
         if(user === ""){
@@ -80,16 +83,27 @@ export default function Dashboard(){
             {loading && <video width="50%" height="50%" style={{display: "grid", margin: "auto", marginTop: "150px"}} autoPlay loop muted playsInline >
                             <source src={video1} type="video/mp4"/></video>}
         {!loading && <div>
-            <p style={{marginLeft: "20px"}}>Welcome back <span style={{color: "#2699c7", fontSize: "larger", fontWeight: "bolder", marginBottom: "-100px"}}>{user}</span>!</p>
-            { game.length!==undefined && game.length!==0 && <p style={{fontWeight: "bold", fontSize: "30px", marginLeft: "20px", marginRight: "20px"}}>Your games</p>}
-            <Link to={`/profile/${user}`} target="_blank"><AccountCircleIcon titleAccess="Account" style={{color: "#2699c7", position: "fixed", right: "98px", top: "15px"}}/></Link>
-            <MoveToInboxIcon titleAccess="Inbox" onClick={()=>(navigate(("/requests")))} style={{position: "fixed", right: "25px", top: "15px", color: "#2699c7"}}/>
-            <p style={{position: "fixed", color: "white", top: "-10px", right: "7px", border: "solid 2px red", borderRadius: "50%", height: "20px", width: "20px", background: "red", fontSize: "12px"}}><span style={{paddingRight: "10px", marginLeft: "4px"}}>{requests}</span></p>
+            <div className="dashboardLeft" id="animatedDashboard">
+                {!expandClick && <ArrowCircleRightIcon style={{position: "absolute", left:"56px", titleAccess: "Expand", cursor: "pointer"}} onClick={()=>{var element = document.getElementById("animatedDashboard"); element.classList.add("activeClick"); var ele = document.getElementById("animatedDashboardRight"); ele.classList.add("activeClickRight"); setExpandClicked(true)}}/>}
+                {expandClick && <ArrowCircleLeftIcon style={{position: "absolute", left: "145px", titleAccess: "Collapse", cursor: "pointer"}} onClick={()=>{var element = document.getElementById("animatedDashboard"); element.classList.remove("activeClick"); var ele = document.getElementById("animatedDashboardRight"); ele.classList.remove("activeClickRight"); setExpandClicked(false)}}/>}
+            <Link to={`/profile/${user}`} target="_blank"><AccountCircleIcon titleAccess="Account" style={{color: "white", position: "fixed", left: "14px", top: "15px", fontSize: "25px", cursor: "pointer"}}/></Link>
+            {expandClick && <p style={{color: "white", marginLeft: "56px"}}>Account</p>}
+            <MoveToInboxIcon titleAccess="Inbox" onClick={()=>(navigate(("/requests")))} style={{position: "fixed", left: "14px", top: "65px", color: "white", fontSize: "25px", cursor: "pointer"}}/>
+            <p style={{position: "fixed", color: "white", top: "40px", left: "25px", border: "solid 2px red", borderRadius: "50%", height: "20px", width: "20px", background: "red", fontSize: "12px", cursor: "pointer"}}><span style={{paddingRight: "10px", marginLeft: "4px"}}>{requests}</span></p>
+            {expandClick && <p style={{color: "white", marginLeft: "56px", marginTop: "30px"}}>Inbox</p>}
+
             <LogoutIcon titleAccess="logout" onClick={()=> {
                 cookies.remove('TicTacToe');
                 setUser("");
                 navigate("/")
-            }} style={{position: "fixed", right: "60px", top: "15px", color: "#2699c7"}}/>
+            }} style={{position: "fixed", right: "60px", bottom: "15px", color: "white", left: "14px", cursor: "pointer"}}/>
+            {expandClick && <p style={{color: "white", marginLeft: "56px", position: "absolute", bottom: "2px"}}>Logout</p>}
+
+            </div>
+            <div className="dashboardRight" id="animatedDashboardRight">
+            <p style={{marginLeft: "20px", marginTop: "25px"}}>Welcome back <span style={{color: "#2699c7", fontSize: "larger", fontWeight: "bolder", marginBottom: "-100px"}}>{user}</span>!</p>
+            { game.length!==undefined && game.length!==0 && <p style={{fontWeight: "bold", fontSize: "30px", marginLeft: "20px", marginRight: "20px"}}>Your games</p>}
+            
             {game.length === 0 && <div>
                 <p className="dashBoardNogames">No Games Found</p>
                 <p className="mainLogin" onClick={()=>{navigate("/newgame")}} style={{marginTop: "-50px"}}><span className="mainLoginText" style={{marginLeft: "-30px"}}>Create new game</span></p>
@@ -97,7 +111,7 @@ export default function Dashboard(){
         {game!==[] && game.length !== 0 && <div className="gameFlex" style={{marginTop: "20px", marginBottom: "25px"}}>{game.map((key)=>(
             <Singlegame user1 = {key.user1} user2 = {key.user2} current = {key.current}  winby = {key.winby} board = {key.board} time = {key.time} id = {key._id} winpo = {key.winpo}/>
         ))}<p className="newGameText" onClick={()=>(navigate("/newgame"))}><span style={{color: "white", cursor: "pointer"}}>+ New Game</span></p></div>}
-        </div>}
+        </div></div>}
         </div>
     )
 }
