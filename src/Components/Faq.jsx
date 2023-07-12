f4f3e5d7e8d38270d3054ac8efd639075f151cbe
HerokuNivas <116030619+HerokuNivas@users.noreply.github.com>
2023-07-12 11:44:54 +0530

    Update Faq.jsx

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -11,7 +11,7 @@ export default function Faq(){
             then there is <span style={{color: "#2699c7"}}>New Game</span> option at bottom right corner, add your friend's email and send request.</p>
             <p style={{fontSize: "20px", color: "red"}}>3) Though I sent request why is the game not visible in dashboard?</p>
             <p style={{fontSize: "22px", color: "black"}}>A) Your friend needs to accept the request to create a game and get started. If the game is still not visible even after accepting the request then please drop a mail to us at
-            <a href="mailto:tictactoeasync@gmail.com" style={{textDecoration: "none", color: "inherit"}}><span style={{color: "#2699c7"}}> tictactoeasync@gmail.com</span></a>.</p>
+            <a href="mailto:tictactoeasync@gmail.com" style={{textDecoration: "none", color: "inherit"}}><span style={{color: "#2699c7"}}> vsnsainivasand2003@gmail.com</span></a>.</p>
             <p style={{fontSize: "20px", color: "red"}}>4) Can I play different games with different users at same time?</p>
             <p style={{fontSize: "22px", color: "black"}}>A) Yes, you can do that. In dashboard <span style={{color: "#2699c7"}}>Click all the games you need to play</span>, they will be opening in different tabs which allows you to play the games simultaneously with different users.</p>
             <p style={{fontSize: "20px", color: "red"}}>5) How can I send request to AI?</p>
